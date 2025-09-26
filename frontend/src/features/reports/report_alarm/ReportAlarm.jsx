b1d0c0f3764e9e9d1ba3b2b9d491f1c563e7f88b
abhiroy00 <abhi7464099@gmail.com>
2025-09-26 14:37:40 +0530

    i added somthing
    i added somthing

--- frontend/src/features/reports/report_alarm/ReportAlarm.jsx
+++ frontend/src/features/reports/report_alarm/ReportAlarm.jsx
@@ -4,8 +4,13 @@ export default function ReportAlarm() {
   return (
     <div>
 
-       <div className="w-full mt-3 bg-cyan-400 shadow-2xl rounded-2xl p-8">
-  <h2 className="text-xl font-semibold text-white">Report Alarm</h2>
+<div className="w-full mt-30 shadow-2xl rounded-2xl p-8" style={{backgroundColor:"#d8eefc"}}>
+  <div className='flex justify-between'>
+  <p className='font-bold '>Station : Pt Ram Prasad Bismil (PRPM)</p>
+  <p className='font-bold '>Division : Moradabad</p>
+  <p className='font-bold '> Zone : Northern Railway</p>
+  </div>
+ 
 </div>
 
       
--- frontend/src/features/reports/report_analaysis/ReportAnalaysis.jsx
+++ frontend/src/features/reports/report_analaysis/ReportAnalaysis.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function ReportAnalaysis() {
   return (
     <div>
+   <div className="w-full mt-30 shadow-2xl rounded-2xl p-8" style={{backgroundColor:"#d8eefc"}}>
+     <h1 className='text-red-600 text-5xl'> work in progress</h1>
       
+   </div>
     </div>
   )
 }
