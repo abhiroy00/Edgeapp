1729ce8e51878969a612f70b914e71d353c1a796
NamanSharma123 <139445554+NamanSharma123@users.noreply.github.com>
2025-10-02 10:20:35 +0530

    update fonts and symbols
    update fonts and symbols

--- frontend/src/features/user/user_type/UserType.jsx
+++ frontend/src/features/user/user_type/UserType.jsx
@@ -55,9 +55,9 @@ export default function UserType() {
 
   return (
     <div className="w-full mt-30 min-h-screen bg-gray-100 flex justify-center py-10">
-      <div className="w-full max-w-4xl bg-white shadow-xl rounded-lg p-8">
-        <h1 className="text-3xl font-bold text-blue-700 mb-5 text-center">
-          User Types
+      <div className="w-full max-w-7xl bg-white shadow-xl rounded-lg p-8">
+        <h1 className="text-3xl font-bold text-[oklch(0.53_0.27_303.85)] mb-5">
+        🧑‍🌾 User Types
         </h1>
 
         {/* Search */}
@@ -105,7 +105,7 @@ export default function UserType() {
           <div className="flex items-end">
             <button
               type="submit"
-              className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded shadow-md transition"
+              className="bg-[oklch(0.48_0.27_303.85)] hover:bg-[oklch(0.48_0.27_303.85)] text-white px-6 py-2 rounded shadow-md transition"
             >
               {editingId ? "Update User Type" : "Add User Type"}
             </button>
