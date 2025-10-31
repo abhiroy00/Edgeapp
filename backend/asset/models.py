2b776a1b3c1ea43826cb44bbd92f41f187437762
NamanSharma123 <139445554+NamanSharma123@users.noreply.github.com>
2025-10-31 20:00:24 +0530

    created assest inventory model and serializer
    created assest inventory model and serializer

--- backend/asset/models.py
+++ backend/asset/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from junction.models import junctionboxmaster
 
 # Create your models here.
 
@@ -10,4 +11,21 @@ class AssetMaster(models.Model):
 
     def __str__(self):
         return f"{self.assetid}"
+    
+class AsserInventory(models.Model):
+    assetinventoryid=models.AutoField(primary_key=True)
+    assetid=models.ForeignKey(AssetMaster,on_delete=models.CASCADE,related_name='assetmaster')
+    junctionid=models.ForeignKey(junctionboxmaster,on_delete=models.CASCADE,related_name='junctionboxmaster')
+    manufacturermodel=models.CharField(max_length=200)
+    manufactureddate=models.DateField()
+    manufactureyear=models.IntegerField()
+    serialnumber=models.CharField(max_length=200)
+    lattitude=models.CharField(max_length=200)
+    longitude=models.CharField(max_length=200)
+    railwaycode=models.CharField(max_length=200)
+    isRDPMSasset=models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return f"{self.assetinventoryid}"
 
--- backend/asset/serializer.py
+++ backend/asset/serializer.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from asset.models import AssetMaster
+from asset.models import AssetMaster,AssetInventory
+from junction.models import junctionboxmaster
 
 class AssetMasterSerializer(serializers.ModelSerializer):
     class Meta:
         model=AssetMaster
         fields='__all__'
+
+
+class AssetInventorySerializer(serializers.ModelSerializer):
+    junctionid = serializers.PrimaryKeyRelatedField(queryset=junctionboxmaster.objects.all())
+    class Meta:
+        model = AssetInventory
+        fields = '__all__'
